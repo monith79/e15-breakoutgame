2332f32928ff725b020444b477ce626ed0208b3e
monith79 <un.monith168@gmail.com>
2025-06-29 08:21:19 +0700

    E8-Add MultiBall

--- Assets/Script/BallsManager.cs
+++ Assets/Script/BallsManager.cs
@@ -102,4 +102,17 @@ public class BallsManager : MonoBehaviour
         }
         InitBall();
     }
+
+    public void SpawnBalls(Vector3 position, int count)
+    {
+        for(int i = 0; i < count; i++)
+        {
+            Ball spawnedBall = Instantiate(ballPrefab, position, Quaternion.identity) as Ball;
+
+            Rigidbody2D spawnedBallRb = spawnedBall.GetComponent<Rigidbody2D>();
+            spawnedBallRb.isKinematic = false;
+            spawnedBallRb.AddForce(new Vector2(0, initialBallSpeed));
+            this.Balls.Add(spawnedBall);
+        }
+    }
 }
---
+++ Assets/Script/Collectables/MultiBall.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class MultiBall : Collectable
+{
+    protected override void ApplyEffect()
+    {
+        foreach (Ball ball in BallsManager.Instance.Balls.ToList())
+        {
+            BallsManager.Instance.SpawnBalls(ball.gameObject.transform.position, 2);
+        }
+    }
+}
